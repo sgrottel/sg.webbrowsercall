4b40cadb659bf8ba722349ead94e25cc6484c62b
sgrottel <webmaster@sgrottel.de>
2020-12-14 08:55:52 +0100

    Added "ExampleWpfApp" to show interactive UI integration.

---
+++ ExampleWpfApp/BrowserInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Media;
+
+namespace ExampleWpfApp
+{
+	public class BrowserInfo
+	{
+		public string Name { get; set; } = "Dummy";
+
+		public ImageSource Icon { get; set; } = (ImageSource)App.Current.Resources["Icon_Web_x32"];
+
+		public bool IsDefault { get; set; } = false;
+
+		public Action<string> Open { get; set; } = null;
+
+		internal bool LoadIcon(string iconInfo)
+		{
+			Match iim = Regex.Match(iconInfo, "^(.*)(?:,[-0-9]+)?$");
+			if (!iim.Success) return false;
+			if (!iim.Groups[1].Success) return false;
+			string filename = iim.Groups[1].Value;
+			if (!System.IO.File.Exists(filename)) return false;
+
+			using (var icon = System.Drawing.Icon.ExtractAssociatedIcon(filename))
+			{
+				Icon = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(
+					icon.Handle,
+					System.Windows.Int32Rect.Empty,
+					System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+			}
+			return true;
+		}
+	}
+}
---
+++ ExampleWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExampleWpfApp
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			List<BrowserInfo> browsers
+				= Array.ConvertAll<SG.WebBrowserCall.WebBrowser, BrowserInfo>(
+					SG.WebBrowserCall.WebBrowser.GetInstalledBrowsers(),
+					(SG.WebBrowserCall.WebBrowser b) =>
+					{
+						var bi = new BrowserInfo()
+						{
+							Name = b.Name,
+							IsDefault = b.IsDefault,
+							Open = (string s) => { b.Open(s); }
+						};
+						if (!bi.LoadIcon(b.IconInfo))
+						{
+							bi.LoadIcon(b.ExecutablePath);
+						}
+						return bi;
+					}
+					).ToList();
+			browsers.Sort((BrowserInfo a, BrowserInfo b) => { return a.Name.CompareTo(b.Name); });
+
+			BrowserButtons.ItemsSource = browsers;
+		}
+
+		private void BrowserButton_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				BrowserInfo browser = (BrowserInfo)((Button)sender).DataContext;
+				browser.Open.Invoke(TextBoxUrl.Text);
+			}
+			catch(Exception ex)
+			{
+				Message.Text = "Failed to invoke WebBrowser: " + ex.ToString();
+			}
+
+		}
+	}
+}
